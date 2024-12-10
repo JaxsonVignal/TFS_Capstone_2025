f36aed836eff2637b9f6cee79ce1edc5d111fc00
sourceofentropy <jonathan.r.anthony@gmail.com>
2024-12-10 00:51:37 -0800

    attempt to dynamically get the slider component and initialize slider to current health
    Currently executes but has a timing bug where the currentHealth is still 0 at the time this function is called.

--- TFS_Capstone_2025/Assets/Scripts/GameMechanics/EnemyController.cs
+++ TFS_Capstone_2025/Assets/Scripts/GameMechanics/EnemyController.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(HealthSystem))]
 public class EnemyController : MonoBehaviour
 {
-    private HealthSystem healthSystem;
-    private GameObject healthSlider;
-    [SerializeField] private GameObject healthSliderAttachPoint;
+    //TODO: #CodeReview: To Ryan. I get the sense that the coupling between the healthSystem, healthSlider, and this controller is a bit of a mess. I would love if you would rip it to shreds with examples of how to better organize this. Thank you good sir. Though I'm frightened you'll tell me it's over complicated and show me some dirt simple way to do it instead :) 
     
+    private HealthSystem healthSystem; //system for tracking health and damage
+    private GameObject healthSlider; //healthSlider Prefab to instantiate 
+    [SerializeField] private GameObject healthSliderAttachPoint; //location to attach healthSlider prefab
+    private Slider uiHealthSliderBar; //portion of health slider to update with current health
+    [SerializeField] private string healthSliderTag = "SliderBar";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +22,23 @@ public class EnemyController : MonoBehaviour
             Debug.LogError("Assign health slider attach point to enemy controller for " + this.name); 
         }
 
+        healthSystem = GetComponent<HealthSystem>();
+        healthSystem.onDeath.AddListener(OnEnemyDeath);
+        healthSystem.onHealthChanged.AddListener(OnHealthChanged);
+
         //instantiate generic slider prefab as health slider
         //create it at the position of the attach point (is this redundant when making it a child of the attach point?) and its rotation
         //make it a child of the attach point
-        healthSlider = Instantiate(GameManager.Instance.sliderPrefab, healthSliderAttachPoint.transform.position, transform.rotation, healthSliderAttachPoint.transform); 
+        healthSlider = Instantiate(GameManager.Instance.sliderPrefab, healthSliderAttachPoint.transform.position, transform.rotation, healthSliderAttachPoint.transform);
+        uiHealthSliderBar = GetComponentInChildren<Slider>();
+        if (uiHealthSliderBar == null)
+        {
+            Debug.LogError("cannot access slider value for health bar on enemy " + this.name);
+        }
+        Debug.Log("Current health % = " + healthSystem.GetHealthPercentage());
+        Debug.Log("Current healthbar value = " + uiHealthSliderBar.value);
+        uiHealthSliderBar.value = healthSystem.GetHealthPercentage(); //init value to 100%
 
-        healthSystem = GetComponent<HealthSystem>();
-        healthSystem.onDeath.AddListener(OnEnemyDeath);
     }
 
     // Update is called once per frame
@@ -43,6 +58,31 @@ public class EnemyController : MonoBehaviour
     {
         // Handle enemy death (e.g., play death animation, destroy enemy)
         Debug.Log("Enemy has died!");
+    }
+
+    void OnHealthChanged()
+    {
+        uiHealthSliderBar.value = healthSystem.GetHealthPercentage();
+    }
+
+    //helper to find slider value portion of health slider
+    GameObject FindChildWithTag(Transform parent, string tag)
+    {
+        foreach (Transform child in parent)
+        {
+            if (child.CompareTag(tag))
+            {
+                return child.gameObject;
+            }
+
+            // Recursively search through the child's children
+            GameObject result = FindChildWithTag(child, tag);
+            if (result != null)
+            {
+                return result;
+            }
+        }
+        return null;
     }
 
 }
